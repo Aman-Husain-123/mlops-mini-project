b5c3ce55ee92ee9edc592593a991c7593b015a97
Aman <amanhusain8899@gmail.com>
2026-06-12 09:56:04 +0530

    Add Experiment 2 BOW vs TFIDF

--- notebooks/dags_hub_setup.py
+++ notebooks/dags_hub_setup.py
@@ -1,7 +1,7 @@
 import mlflow
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/Aman-Husain-123/mlops-mini-project.mlflow ')
+mlflow.set_tracking_uri('https://dagshub.com/Aman-Husain-123/mlops-mini-project.mlflow')
 dagshub.init(repo_owner='Aman-Husain-123', repo_name='mlops-mini-project', mlflow=True)
 
 import mlflow
