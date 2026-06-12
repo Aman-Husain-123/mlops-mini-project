83cfd1dea71dfb098306c70a1694a3a6921918b2
Aman <amanhusain8899@gmail.com>
2026-06-12 09:19:20 +0530

    Add EXP-1-logisitic regression

---
+++ notebooks/dags_hub_setup.py
@@ -0,0 +1,10 @@
+import mlflow
+import dagshub
+
+mlflow.set_tracking_uri('https://dagshub.com/Aman-Husain-123/mlops-mini-project.mlflow ')
+dagshub.init(repo_owner='Aman-Husain-123', repo_name='mlops-mini-project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
